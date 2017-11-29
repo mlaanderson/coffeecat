cd5dfa16e9038c61984221d6679d4111358daed0
Michael Anderson <Michael.Anderson@amesburytruth.com>
2017-11-29 17:05:46 -0600

    Fixed an error in picking the configuration file path

--- bin/app.js
+++ bin/app.js
@@ -11,7 +11,7 @@ program
     .option('-c, --conf <filename>', 'specify a configuration file')
     .parse(process.argv);
 
-program.conf = path.resolve(program.conf) || path.resolve(path.join('.', 'conf', 'server.json')); 
+program.conf = path.resolve(program.conf || path.join('.', 'conf', 'server.json')); 
 
 var config = require(program.conf);
 
