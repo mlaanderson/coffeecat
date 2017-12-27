b647649227fd550daef5478f9ac01c11df09d9e4
Michael Anderson <Michael.Anderson@amesburytruth.com>
2017-12-27 15:53:20 -0600

    Fixed conf location search for running as Windows Service

--- bin/defaults.js
+++ bin/defaults.js
@@ -11,6 +11,14 @@ module.exports = function defaults() {
         applets: path.join(os.homedir(), ".coffeecat", "applets")
     }
 
+    if ((user.username === 'SYSTEM') && (os.platform() === 'win32')) {
+        // running as the system user, probably means we're a service
+        // look for the configuration file where coffeecat is installed
+        let homedir = process.cwd().split(path.sep).filter((s,n,a) => n < a.indexOf('AppData')).join(path.sep);
+        result.configuration = path.join(homedir, ".coffeecat", "conf", "server.json");
+        result.applets = path.join(homedir, ".coffeecat", "applets");
+    }
+
     if ((user.uid === 0) || (user.username === 'www-data')) {
         // default to /etc and /var/coffeecat
         result.configuration = path.join('/', 'etc', 'coffeecat', 'server.json');
